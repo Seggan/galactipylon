86e4b6e8c91530bbefe1087c90f854133a8a6065
Seggan <seggan21@gmail.com>
2025-11-17 13:18:01 -0500

    Initial commit

---
+++ src/main/kotlin/io/github/seggan/galactipylon/Bootstrapper.kt
@@ -0,0 +1,13 @@
+package io.github.seggan.galactipylon
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap
+import io.papermc.paper.plugin.bootstrap.PluginProviderContext
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("UnstableApiUsage")
+class Bootstrapper : PluginBootstrap {
+    override fun bootstrap(context: BootstrapContext) {}
+
+    override fun createPlugin(context: PluginProviderContext): JavaPlugin = Galactipylon
+}
---
+++ src/main/kotlin/io/github/seggan/galactipylon/Galactipylon.kt
@@ -0,0 +1,14 @@
+package io.github.seggan.galactipylon
+
+import org.bukkit.plugin.java.JavaPlugin
+
+object Galactipylon : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
